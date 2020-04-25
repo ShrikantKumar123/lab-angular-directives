16ccacab9d823f5f5ae7bcbfe555bf29befe827f
unknown <shrikant2994anil@gmail.com>
2020-04-25 22:22:18 +0530

    FSJ0320A005

---
+++ directive-ngfor-app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  productList: Array<string> = [
+    'I phone',
+    'Galaxy 9.0',
+    'BlackBerrry 10z',
+    'Mi',
+    'Oppo'
+  ]
+}
